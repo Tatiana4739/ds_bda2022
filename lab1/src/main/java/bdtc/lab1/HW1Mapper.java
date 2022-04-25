d6212e13042690c8f4a813eab1a132f84744707d
volkovati <tivolkova00@mail.ru>
2022-04-25 12:13:43 -0700

    init

---
+++ lab1/src/main/java/bdtc/lab1/HW1Mapper.java
@@ -0,0 +1,108 @@
+package bdtc.lab1;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import java.util.stream.Stream;
+
+
+public class HW1Mapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+    private final static IntWritable one = new IntWritable(1);
+    private final Text word = new Text();
+    private final Pattern digitCodePattern = Pattern.compile("[\\d]+-[\\d]+-[\\d]+:");
+    private final Pattern linuxCodeLog = Pattern.compile("-[0-7]-");
+    private HashMap<String, String> wordDefineCode = null;
+
+    @Override
+    protected void setup(Context context) throws IOException, InterruptedException {
+        wordDefineCode = new HashMap<>();
+
+        URI[] cacheFiles = context.getCacheFiles();
+        if (cacheFiles != null && cacheFiles.length > 0) {
+            try (Stream<String> stream = Files.lines(Paths.get(cacheFiles[0]))) {
+                stream.forEach((line) -> {
+                    String[] splinted = line.split("-", 2);
+                    wordDefineCode.put(splinted[0], splinted[1]);
+                });
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String line = value.toString();
+        Matcher matcher = digitCodePattern.matcher(line);
+
+        if (!matcher.find()) {
+            context.getCounter(CounterType.MALFORMED).increment(1);
+        } else {
+            String partLog = matcher.group();
+            Matcher codeMatcher = linuxCodeLog.matcher(partLog);
+
+            if (!codeMatcher.find()) {
+                context.getCounter(CounterType.MALFORMED).increment(1);
+            } else {
+                String linuxCode = codeMatcher.group().replaceAll("[-]", "");
+                word.set(wordDefineCode.get(linuxCode));
+                context.write(word, one);
+            }
+        }
+    }
+}
+
+/*
+package bdtc.lab1;
+
+import eu.bitwalker.useragentutils.Browser;
+import eu.bitwalker.useragentutils.UserAgent;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class HW1Mapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+    private final static IntWritable one = new IntWritable(1);
+    private final Text word = new Text();
+    private final Pattern digitCodePattern = Pattern.compile("[\\d]+-[\\d]+-[\\d]+:");
+    private final Pattern linuxCodeLog = Pattern.compile("-[0-7]-");
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String line = value.toString();
+        Matcher matcher = digitCodePattern.matcher(line);
+
+        if (!matcher.find()) {
+            context.getCounter(CounterType.MALFORMED).increment(1);
+        } else {
+            String partLog = matcher.group();
+            Matcher codeMatcher = linuxCodeLog.matcher(partLog);
+
+            if (!codeMatcher.find()) {
+                context.getCounter(CounterType.MALFORMED).increment(1);
+            } else {
+                String linuxCode = codeMatcher.group().replaceAll("[-]", "");
+                word.set(linuxCode);
+                context.write(word, one);
+            }
+        }
+    }
+}
+*/
---
+++ lab1/src/main/java/bdtc/lab1/MapReduceApplication.java
@@ -0,0 +1,141 @@
+package bdtc.lab1;
+
+import lombok.extern.log4j.Log4j;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.SequenceFile;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Counter;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
+
+import java.io.IOException;
+import java.net.URI;
+
+
+@Log4j
+public class MapReduceApplication {
+
+    public static void main(String[] args) throws Exception {
+
+        if (args.length < 2) {
+            throw new RuntimeException("You should specify input and output folders!");
+        }
+        Configuration conf = new Configuration();
+        conf.set("mapreduce.map.output.compress", "true");
+        conf.set("mapred.map.output.compress.codec", "org.apache.hadoop.io.compress.SnappyCodec");
+
+        Job job = Job.getInstance(conf, "logs alert count");
+        job.setJarByClass(MapReduceApplication.class);
+        job.setMapperClass(HW1Mapper.class);
+        job.setReducerClass(HW1Reducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        job.setOutputFormatClass(SequenceFileOutputFormat.class);
+        //conf.set("mapred.output.compress", "true");
+        //conf.set("mapred.output.compression.codec", "org.apache.hadoop.io.compress.SnappyCodec");
+        try {
+            job.addCacheFile(new URI("file://" + "/home/qqq/lab1/matcher.txt"));
+        }
+        catch (Exception e) {
+            System.out.println("Failed add cache");
+            System.exit(1);
+        }
+
+        Path outputDirectory = new Path(args[1]);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, outputDirectory);
+        log.info("=====================JOB STARTED=====================");
+        job.waitForCompletion(true);
+        log.info("=====================JOB ENDED=====================");
+        // проверяем статистику по счётчикам
+        Counter counter = job.getCounters().findCounter(CounterType.MALFORMED);
+        log.info("=====================COUNTERS " + counter.getName() + ": " + counter.getValue() + "=====================");
+
+        readSeqFile(new Path(outputDirectory, "part-r-00000"));
+    }
+
+    private static void readSeqFile(Path pathToFile) throws IOException {
+
+        /*
+
+        Функция чтения SequenceFile после выполения MapReduce Job
+
+         */
+
+        Configuration conf = new Configuration();
+
+        FileSystem fs = FileSystem.get(conf);
+
+
+
+
+        SequenceFile.Reader reader = new SequenceFile.Reader(fs, pathToFile, conf);
+
+
+
+
+        Text key = new Text();
+
+        IntWritable val = new IntWritable();
+
+        while (reader.next(key, val)) {
+
+            log.info(key + ":" + val);
+
+        }
+
+    }
+}
+
+/*
+package bdtc.lab1;
+
+import lombok.extern.log4j.Log4j;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Counter;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+
+@Log4j
+public class MapReduceApplication {
+
+    public static void main(String[] args) throws Exception {
+
+        if (args.length < 2) {
+            throw new RuntimeException("You should specify input and output folders!");
+        }
+        Configuration conf = new Configuration();
+        // задаём выходной файл, разделенный запятыми - формат CSV в соответствии с заданием
+        conf.set("mapreduce.output.textoutputformat.separator", ",");
+
+        Job job = Job.getInstance(conf, "browser count");
+        job.setJarByClass(MapReduceApplication.class);
+        job.setMapperClass(HW1Mapper.class);
+        job.setReducerClass(HW1Reducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+
+        Path outputDirectory = new Path(args[1]);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, outputDirectory);
+        log.info("=====================JOB STARTED=====================");
+        job.waitForCompletion(true);
+        log.info("=====================JOB ENDED=====================");
+        // проверяем статистику по счётчикам
+        Counter counter = job.getCounters().findCounter(CounterType.MALFORMED);
+        log.info("=====================COUNTERS " + counter.getName() + ": " + counter.getValue() + "=====================");
+    }
+}
+*/
